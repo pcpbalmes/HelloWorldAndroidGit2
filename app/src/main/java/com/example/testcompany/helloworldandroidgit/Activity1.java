86533880d23a022149b6a7e07cc425214ae631d5
pcp <pcomellas@jaumebalmes.net>
2018-09-05 12:59:28 +0200

    aaa

---
+++ app/src/main/java/com/example/testcompany/helloworldandroidgit/Activity1.java
@@ -0,0 +1,15 @@
+package com.example.testcompany.helloworldandroidgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+        aaa
+                
+    }
+}
